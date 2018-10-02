0079b1092bc938e5f88f65d8f566b2eaa0e22a47
Max Kohler <max@koehler-kn.de>
2018-10-03 00:12:44 +0100

    Block in content"

--- dist/main.js
+++ dist/main.js
@@ -1,3 +1,11 @@
+const gra = function(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+const gri = function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 const initPlayer = function (element) {
     console.log('Initializing player')
     let playBtn = element.querySelector('#play-btn')
@@ -15,6 +23,54 @@ const initPlayers = function(){
   }
 }
 
+const setTransforms = function(){
+    console.log('Setting transforms..')
+    let r = window.innerWidth * .35;
+    let ry = window.innerHeight * .35;
+	let letters = document.querySelectorAll('.tracks-item')
+	let n_letters = letters.length;
+	for (let i = 0; i < n_letters; i++){
+		let n = (360 / n_letters) * i;
+		let rotateY = n * -1;
+		let translateX = (Math.sin(((n / 360) * 2 * Math.PI) + Math.PI) * -r) + 30
+		let translateY = Math.cos((n/360) * 2 * Math.PI) * ry;
+		//translateZ = 0;
+		//translateX = 0;
+        rotateY = 0;
+        let inner = letters[i].querySelector('.track-inner')
+        inner.style.animationDuration = gra(3600, 5900) + 'ms, ' + gra(1200, 2000) + 'ms';
+        inner.style.animationDelay = gra(-500, 400) + 'ms, ' + gra(500, 1500) + 'ms';
+		letters[i].style.transform = 'translateX(calc(' + translateX + 'px - 50%)) translateY(calc(' + translateY + 'px - 50%)) rotateZ(' + rotateY + 'deg)';
+	}
+}
+
+
 document.addEventListener('DOMContentLoaded', function () {
     initPlayers();
 })
+
+let last_known_scroll_position = 0;
+let ticking = false;
+
+let topSection = document.querySelector('.top');
+
+function doSomething(scroll_pos) {
+    topSection.style.filter = 'blur(' + scroll_pos / 20 + 'px)'
+}
+
+
+window.addEventListener('scroll', function(e) {
+    last_known_scroll_position = window.scrollY;
+    if (!ticking) {
+        window.requestAnimationFrame(function() {
+            doSomething(last_known_scroll_position);
+            ticking = false;
+        });
+        ticking = true;
+    }
+    
+});
+
+window.addEventListener('resize', setTransforms)
+
+setTransforms()
